bbfd9335f79e1ad1f333d339ef93abbe6f34b385
czy <503438078@qq.com>
2017-05-05 15:12:43 +0800

    添加图表折线

--- app/src/main/java/com/example/czy/petrolconsume/fragment/OilConsumeFragment.java
+++ app/src/main/java/com/example/czy/petrolconsume/fragment/OilConsumeFragment.java
@@ -1,5 +1,6 @@
 package com.example.czy.petrolconsume.fragment;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -7,6 +8,18 @@ import android.widget.TextView;
 
 import com.example.czy.petrolconsume.R;
 import com.example.czy.petrolconsume.base.BaseFragment;
+import com.example.czy.petrolconsume.bean.RecordBean;
+import com.example.czy.petrolconsume.database.RxSQLite;
+import com.example.czy.petrolconsume.tool.LinearChartView;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by CZY on 2017/4/18.
@@ -16,8 +29,30 @@ public class OilConsumeFragment extends BaseFragment {
 
     private LinearLayout linearLayout;
     private int count = 0;
-    private TextView textTop;
-    private ImageView pointIv,pointIv1,pointIv2,pointIv3,pointIv4,leftIv,rightIv;
+    private TextView textTop, oilFirstTv;
+    private ImageView pointIv, pointIv1, pointIv2, pointIv3, pointIv4, leftIv, rightIv;
+    private LinearChartView customView;
+    private float[] consume;
+    private int[] odometer;
+    private float[] consumeA;
+    private float[] price;
+    private long[] dates;
+    private float[] oilConsumption = new float[]{2, 10, 17, 11, 5, 8, 15, 20, 14, 8, 19, 4, 9, 12, 8, 5, 18, 8, 12, 5, 10, 6, 4, 15, 8, 9, 27, 6, 22, 15, 4, 14, 28, 5, 7};
+    private float[] yearConsumption = new float[]{5, 11, 3, 9, 12, 25, 7, 17, 22, 8, 13, 27};
+    private float[] halfConsumption = new float[]{6, 11, 25, 9, 5, 13};
+    private float[] threeConsumption = new float[]{4, 28, 19};
+
+    public static String getYearToString(long time) {
+        Date d = new Date(time);
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy");
+        return sf.format(d);
+    }
+
+    public static String getMonthToString(long time) {
+        Date d = new Date(time);
+        SimpleDateFormat sf = new SimpleDateFormat("MM");
+        return sf.format(d);
+    }
 
     @Override
     protected int bindLayout() {
@@ -30,10 +65,16 @@ public class OilConsumeFragment extends BaseFragment {
         textTop = bindView(R.id.text_top);
         leftIv = bindView(R.id.left_oil_first);
         rightIv = bindView(R.id.right_oil_first);
+        oilFirstTv = bindView(R.id.oil_first_tv);
+        customView = bindView(R.id.custom_view);
     }
 
     @Override
     protected void initData() {
+        selectedRecords();
+        customView.setCOUNT_X_MARK(35);
+        customView.setConsume(oilConsumption);
+
         for (int i = 0; i < 5; i++) {
             pointIv = new ImageView(getContext());
             pointIv.setPadding(0, 0, 0, 0);
@@ -49,91 +90,21 @@ public class OilConsumeFragment extends BaseFragment {
         rightIv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (count){
+                switch (count) {
                     case 0:
-                        count = 1;
-                        textTop.setText("最近一年油耗统计曲线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv1 = new ImageView(getContext());
-                            pointIv1.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv1.setLayoutParams(params);
-                            if (i == 1) {
-                                pointIv1.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv1.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv1);
-                        }
+                        one();
                         break;
                     case 1:
-                        count = 2;
-                        textTop.setText("最近半年油耗统计曲线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv2 = new ImageView(getContext());
-                            pointIv2.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv2.setLayoutParams(params);
-                            if (i == 2) {
-                                pointIv2.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv2.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv2);
-                        }
+                        two();
                         break;
                     case 2:
-                        count = 3;
-                        textTop.setText("最近三个月油耗统计曲线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv3 = new ImageView(getContext());
-                            pointIv3.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv3.setLayoutParams(params);
-                            if (i == 3) {
-                                pointIv3.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv3.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv3);
-                        }
+                        three();
                         break;
                     case 3:
-                        count = 4;
-                        textTop.setText("同城车型油耗基准线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv4 = new ImageView(getContext());
-                            pointIv4.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv4.setLayoutParams(params);
-                            if (i == 4) {
-                                pointIv4.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv4.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv4);
-                        }
+                        four();
                         break;
                     case 4:
-                        count = 0;
-                        textTop.setText("油耗统计曲线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv = new ImageView(getContext());
-                            pointIv.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv.setLayoutParams(params);
-                            if (i == 0) {
-                                pointIv.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv);
-                        }
+                        zero();
                         break;
                 }
             }
@@ -141,94 +112,150 @@ public class OilConsumeFragment extends BaseFragment {
         leftIv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (count){
+                switch (count) {
                     case 0:
-                        count = 4;
-                        textTop.setText("同城车型油耗基准线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv4 = new ImageView(getContext());
-                            pointIv4.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv4.setLayoutParams(params);
-                            if (i == 4) {
-                                pointIv4.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv4.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv4);
-                        }
+                        four();
                         break;
                     case 4:
-                        count = 3;
-                        textTop.setText("最近三个月油耗统计曲线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv3 = new ImageView(getContext());
-                            pointIv3.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv3.setLayoutParams(params);
-                            if (i == 3) {
-                                pointIv3.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv3.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv3);
-                        }
+                        three();
                         break;
                     case 3:
-                        count = 2;
-                        textTop.setText("最近半年油耗统计曲线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv2 = new ImageView(getContext());
-                            pointIv2.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv2.setLayoutParams(params);
-                            if (i == 2) {
-                                pointIv2.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv2.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv2);
-                        }
+                        two();
                         break;
                     case 2:
-                        count = 1;
-                        textTop.setText("最近一年油耗统计曲线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv1 = new ImageView(getContext());
-                            pointIv1.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv1.setLayoutParams(params);
-                            if (i == 1) {
-                                pointIv1.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv1.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv1);
-                        }
+                        one();
                         break;
                     case 1:
-                        count = 0;
-                        textTop.setText("油耗统计曲线");
-                        linearLayout.removeAllViewsInLayout();
-                        for (int i = 0; i < 5; i++) {
-                            pointIv = new ImageView(getContext());
-                            pointIv.setPadding(0, 0, 0, 0);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-                            pointIv.setLayoutParams(params);
-                            if (i == 0) {
-                                pointIv.setImageResource(R.mipmap.point_white);
-                            } else {
-                                pointIv.setImageResource(R.mipmap.point_grey);
-                            }
-                            linearLayout.addView(pointIv);
-                        }
+                        zero();
                         break;
                 }
             }
         });
+    }
+
+    public void selectedRecords() {
+        RxSQLite.querySelectedRecord()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<RecordBean>>() {
+                    @Override
+                    public void accept(@NonNull List<RecordBean> recordsEntities) throws Exception {
+                        dates = new long[recordsEntities.size()];
+                        for (int i = 0; i < recordsEntities.size(); i++) {
+                            dates[i] = recordsEntities.get(i).getDate();
+                            getYearToString(dates[i]);
+                            Log.d("111", getMonthToString(dates[i]));
+                        }
+                        consume = new float[recordsEntities.size()];
+                        odometer = new int[recordsEntities.size()];
+                        consumeA = new float[recordsEntities.size()];
+                        price = new float[recordsEntities.size()];
+                        for (int i = 0; i < recordsEntities.size(); i++) {
+                            price[i] = (float) recordsEntities.get(i).getPrice();
+                            consume[i] = (float) recordsEntities.get(i).getYuan();
+                            odometer[i] = recordsEntities.get(i).getOdometer();
+                        }
+                    }
+                });
+    }
+
+    public void one() {
+        customView.setCOUNT_X_MARK(12);
+        customView.setConsume(yearConsumption);
+        count = 1;
+        textTop.setText("最近一年油耗统计曲线");
+        linearLayout.removeAllViewsInLayout();
+        for (int i = 0; i < 5; i++) {
+            pointIv1 = new ImageView(getContext());
+            pointIv1.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
+            pointIv1.setLayoutParams(params);
+            if (i == 1) {
+                pointIv1.setImageResource(R.mipmap.point_white);
+            } else {
+                pointIv1.setImageResource(R.mipmap.point_grey);
+            }
+            linearLayout.addView(pointIv1);
+        }
+    }
+
+    public void two() {
+        customView.setCOUNT_X_MARK(6);
+        customView.setConsume(halfConsumption);
+        count = 2;
+        textTop.setText("最近半年油耗统计曲线");
+        linearLayout.removeAllViewsInLayout();
+        for (int i = 0; i < 5; i++) {
+            pointIv2 = new ImageView(getContext());
+            pointIv2.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
+            pointIv2.setLayoutParams(params);
+            if (i == 2) {
+                pointIv2.setImageResource(R.mipmap.point_white);
+            } else {
+                pointIv2.setImageResource(R.mipmap.point_grey);
+            }
+            linearLayout.addView(pointIv2);
+        }
+    }
+
+    private void three() {
+        customView.setCOUNT_X_MARK(3);
+        customView.setConsume(threeConsumption);
+        count = 3;
+        textTop.setText("最近三个月油耗统计曲线");
+        linearLayout.removeAllViewsInLayout();
+        for (int i = 0; i < 5; i++) {
+            pointIv3 = new ImageView(getContext());
+            pointIv3.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
+            pointIv3.setLayoutParams(params);
+            if (i == 3) {
+                pointIv3.setImageResource(R.mipmap.point_white);
+            } else {
+                pointIv3.setImageResource(R.mipmap.point_grey);
+            }
+            linearLayout.addView(pointIv3);
+        }
+    }
+
+    private void four() {
+        customView.setCOUNT_X_MARK(12);
+        customView.setConsume(yearConsumption);
+        count = 4;
+        textTop.setText("同城车型油耗基准线");
+        linearLayout.removeAllViewsInLayout();
+        for (int i = 0; i < 5; i++) {
+            pointIv4 = new ImageView(getContext());
+            pointIv4.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
+            pointIv4.setLayoutParams(params);
+            if (i == 4) {
+                pointIv4.setImageResource(R.mipmap.point_white);
+            } else {
+                pointIv4.setImageResource(R.mipmap.point_grey);
+            }
+            linearLayout.addView(pointIv4);
+        }
+    }
+
+    private void zero() {
+        customView.setCOUNT_X_MARK(35);
+        customView.setConsume(oilConsumption);
+        count = 0;
+        textTop.setText("     油耗统计曲线");
+        linearLayout.removeAllViewsInLayout();
+        for (int i = 0; i < 5; i++) {
+            pointIv = new ImageView(getContext());
+            pointIv.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
+            pointIv.setLayoutParams(params);
+            if (i == 0) {
+                pointIv.setImageResource(R.mipmap.point_white);
+            } else {
+                pointIv.setImageResource(R.mipmap.point_grey);
+            }
+            linearLayout.addView(pointIv);
+        }
     }
 }
